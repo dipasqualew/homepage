c6a9d76e1dd11d60c12e492563456347eaa6c065
William Di Pasquale <wdp@dipasqualew.com>
2023-03-31 03:42:30 +0100

    chore(6): Lint with ESlint (#8)
    Fixes #6

--- src/favourites.ts
+++ src/favourites.ts
@@ -2,17 +2,17 @@ export interface Favourite {
   title: string;
   url: string;
   icon: string;
-};
+}
 
 export interface BigCard {
-  block: "big-card";
+  block: 'big-card';
   favourite: Favourite;
-};
+}
 
 export interface Container {
-  block: "row" | "column" | "root";
+  block: 'row' | 'column' | 'root';
   children: Array<Container | BigCard>
-};
+}
 
 export type Layout = BigCard | Container
 
